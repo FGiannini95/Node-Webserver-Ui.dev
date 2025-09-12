7550ff94f160b196695064aaf9ad6188b821238e
federico <fgiannini95@gmail.com>
2025-09-12 13:33:11 +0100

    Server set up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello TS friends");
+import http, { IncomingMessage, ServerResponse } from "http";
+import path from "path";
+import fs from "fs/promises";
+
+async function requestListener(req: IncomingMessage, res: ServerResponse) {
+  const filePath = path.join(__dirname, "static/index.html");
+
+  const data = await fs.readFile(filePath, "utf-8");
+
+  res.writeHead(200, {
+    "content-type": "text/html",
+    "content-length": data.length,
+  });
+
+  res.write(data);
+  res.end();
+}
+
+http.createServer(requestListener).listen(3000, () => {
+  console.log("HTTP Server listening on port 3000");
+});
